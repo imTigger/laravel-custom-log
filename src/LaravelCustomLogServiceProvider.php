35b4ded9a3376f4e6786d2a487bab5bf66f23852
Tiger Fok <tiger@tiger-workshop.com>
2018-07-07 03:30:43 +0800

    Added tags to vendor:publish

--- src/LaravelCustomLogServiceProvider.php
+++ src/LaravelCustomLogServiceProvider.php
@@ -18,9 +18,12 @@ class LaravelCustomLogServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->publishes([
-            __DIR__ . '/config/custom-log.php' => config_path('custom-log.php'),
-            __DIR__ . '/migrations/2018_07_07_000000_create_logs_table.php' => base_path('database/migrations/2018_07_07_000000_create_logs_table.php'),
-        ]);
+            __DIR__ . '/config/custom-log.php' => config_path('custom-log.php')
+        ], 'config');
+        
+        $this->publishes([
+            __DIR__ . '/migrations/2018_07_07_000000_create_logs_table.php' => base_path('database/migrations/2018_07_07_000000_create_logs_table.php')
+        ], 'migration');
     }
 
 }
